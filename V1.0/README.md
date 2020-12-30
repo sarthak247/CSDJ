c3c09842e3782581de273b0fcb11cbdb1b3499c2
Gopal Kaul <gopalkaul5@gmail.com>
2020-12-30 17:47:34 +0530

    Added stuff fixing Windows issues

---
+++ V1.0/README.md
@@ -0,0 +1,19 @@
+#### CSDJ- An app meant to play Music in Source Games like CSGO
+#### Contributors- sarthak247(Sarthak Thakur), gopal-kaul(Gopal Kaul)
+#### Initial release V1.0
+#### Only CLI interface and one song at a time
+
+This is the initial release of CSDJ!
+
+How to use the app-
+Double click on the csdj.exe file.
+Enter your song name, it will automatically search it from YouTube, download it and copy it to the folder!
+For the first run, it will ask you to paste your CSGO install directory. Paste it till the CSGO folder.
+Example:
+C:\Program Files(x86)\Steam\steamapps\common\Counter-Strike Global Offensive
+~/.steam/steamapps/common/Counter-Strike Global Offensive
+
+In game, run "exec csdj" in console.
+While in game, press '/' to auto play the music, or "'" to push to play music.
+
+Feel free to star, watch this repository for an updated version with GUI soon!
--- downloadandinstall.py
+++ downloadandinstall.py
@@ -22,52 +22,51 @@ def download(name):
     link = search(name)  # Searching the song on YT
     song = pafy.new(link)  # Finding the song using PAFY
     dl = song.getbestaudio()  # Getting the best song and audio quality
-    title = song.title
     path = f"{os.getcwd()}/{name}.webm"
     # Downloading it with output to console
     dl.download(quiet=False, filepath=path)
     #os.rename(title, name)
-    if platform.system()=='Windows':
-    	ffmpeg_cmd = f"{os.path.join(os.getcwd(),'ffmpegw.exe')} -i " + '"' + name + '.webm' + '"' + \
-        	" -acodec pcm_s16le -ar 22050 -ac 1 -fflags +bitexact -flags:v +bitexact -flags:a +bitexact " + f'"{os.path.join(os.getcwd(),"voice_input.wav")}"' +  " -y"
-    	print(ffmpeg_cmd)
-    	subprocess.call(f"{ffmpeg_cmd}",shell=True)
+    if platform.system() == 'Windows':
+        ffmpeg_cmd = f"{os.path.join(os.getcwd(),'ffmpegw.exe')} -i " + '"' + name + '.webm' + '"' + \
+            " -acodec pcm_s16le -ar 22050 -ac 1 -fflags +bitexact -flags:v +bitexact -flags:a +bitexact " + \
+            f'"{os.path.join(os.getcwd(),"voice_input.wav")}"' + " -y"
+        subprocess.call(f"{ffmpeg_cmd}", shell=True)
     # Call to ffmpeg to run the conversion
-    elif platform.system()=="Linux":
-    	ffmpeg_cmd = "./ffmpeglinux -i " + "'" + name + '.webm' + "'" + \
-        	" -acodec pcm_s16le -ar 22050 -ac 1 -fflags +bitexact -flags:v +bitexact -flags:a +bitexact  voice_input.wav -y"
-    	subprocess.call(ffmpeg_cmd, shell=True)
-    
+    elif platform.system() == "Linux":
+        ffmpeg_cmd = f"{os.path.join(os.getcwd(),'ffmpeglinux')} -i " + "'" + name + '.webm' + "'" + \
+            " -acodec pcm_s16le -ar 22050 -ac 1 -fflags +bitexact -flags:v +bitexact -flags:a +bitexact  voice_input.wav -y"
+        subprocess.call(ffmpeg_cmd, shell=True)
 
 
 def movetodir():
     try:
         cfg = open('csdir.cfg')
         dest = str(cfg.read())
-        if platform.system()=='Windows':
-            path = os.path.join(os.getcwd(),'voice_input.wav')
+        path = os.path.join(os.getcwd(), 'voice_input.wav')
+        if platform.system() == 'Windows':
             shutil.copy(path, f"{dest}\\")
-        elif platform.system()=="Linux":
-            shutil.copy(f'{os.getcwd()}/voice_input.wav', f"{dest}/")
+        elif platform.system() == "Linux":
+            shutil.copy(path, f"{dest}")
         print("Copied!")
-        if os.path.exists(os.path.join(dest,'csgo','cfg','csdj.cfg'))==False:
-           shutil.copy(f'{os.path.join(os.getcwd(),"csdj.cfg")}', f"{os.path.join(dest,'csgo','cfg')}")
+        if os.path.exists(os.path.join(dest, 'csgo', 'cfg', 'csdj.cfg')) == False:
+            shutil.copy(f'{os.path.join(os.getcwd(),"csdj.cfg")}',
+                        f"{os.path.join(dest,'csgo','cfg')}")
         cfg.close()
     except FileNotFoundError:  # File does not exist!
         dest = input("Enter your CSGO install dir : ")
         cfg = open('csdir.cfg', 'w')
         cfg.write(dest)
-        if platform.system()=='Windows':
-            path = os.path.join(os.getcwd(),'voice_input.wav')
+        path = os.path.join(os.getcwd(), 'voice_input.wav')
+        if platform.system() == 'Windows':
             shutil.copy(path, f"{dest}\\")
-        elif platform.system()=="Linux":
-            shutil.copy(f'{os.getcwd()}/voice_input.wav', f"{dest}/")
-        if os.path.exists(os.path.join(dest,'csgo','cfg','csdj.cfg'))==False:
-           shutil.copy(f'{os.path.join(os.getcwd(),"csdj.cfg")}', f"{os.path.join(dest,'csgo','cfg')}")
+        elif platform.system() == "Linux":
+            shutil.copy(path, f"{dest}")
         print("Copied!")
+        if os.path.exists(os.path.join(dest, 'csgo', 'cfg', 'csdj.cfg')) == False:
+            shutil.copy(f'{os.path.join(os.getcwd(),"csdj.cfg")}',
+                        f"{os.path.join(dest,'csgo','cfg')}")
         cfg.close()
 
 
 download(input("Enter song you want! : "))
 movetodir()
-
